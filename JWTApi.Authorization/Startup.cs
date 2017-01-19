36a59d9cfb629b1d6b2428f9d18e3cef46b8991b
magicyu90@gmail.com <magicyu90@gmail.com>
2017-01-20 00:13:42 +0100

    JWTAuth-Add Startup

---
+++ JWTApi.Authorization/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(JWTApi.Authorization.Startup))]
+
+namespace JWTApi.Authorization
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            var configuration = new HttpConfiguration();
+
+            //Wep api routes
+            configuration.MapHttpAttributeRoutes();
+
+            ConfigureOAuth(app);
+
+            //support
+            app.Use(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.Use(configuration);
+
+        }
+
+
+        //config OAuthorizaion
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            //todo:config oauth
+
+        }
+    }
+}
