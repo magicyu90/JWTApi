8cc104f1fff1e3ec6c1a3d047d0517a90f8dbd25
magicyu90@gmail.com <magicyu90@gmail.com>
2017-01-20 15:26:54 +0100

    JWTResource-Add resource auth part

---
+++ JWTApi.Resource/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace JWTApi.Resource.Controllers
+{
+
+    [RoutePrefix("api/test")]
+    public class TestController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var res = identity.Claims.Select(c => new
+                {
+                    Type = c.Type,
+                    Value = c.Value
+                });
+
+                return Ok(res);
+            }
+
+            return NotFound();
+
+        }
+    }
+}
---
+++ JWTApi.Resource/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using Microsoft.Owin.Security;
+
+[assembly: OwinStartup(typeof(JWTApi.Resource.Startup))]
+
+namespace JWTApi.Resource
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            var issuer = "http://localhost";
+            var audience = "50de53cc8cf04d469eb433ed117895ae";
+            var secret = TextEncodings.Base64Url.Decode("Yjc0OTM0Mjg5Y2QzNGMwYzgyY2MxZmMwYWUwYzMzNTA=");
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    //todo:check from database
+                    AllowedAudiences = new[] { audience },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                    }
+                });
+
+        }
+    }
+}
